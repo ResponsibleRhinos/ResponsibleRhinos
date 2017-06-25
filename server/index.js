497277d985a280031085b3cc0c06475fd1b26999
dandersonstack <danieltanderson@berkeley.edu>
2017-06-25 12:53:40 -0700

    deleted proc file and changed index.js according to something I found online

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
-
+process.env.PWD = process.cwd() || __dirname;
 var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(process.env.PWD + '/../react-client/dist'));
 
 app.get('/', function(req, res) {
   res.send('hello world!');
-})
+});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
