dbec88f9b264deb159d48ff65b875f90c7ab28aa
dandersonstack <danieltanderson@berkeley.edu>
2017-06-25 12:48:41 -0700

    trying to get deployment working

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
+app.get('/', function(req, res) {
+  res.send('hello world!');
+})
+
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
 });
