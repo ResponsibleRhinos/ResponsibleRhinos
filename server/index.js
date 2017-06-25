2872f0e59f9b34089810a85d3618f867212de11c
dandersonstack <danieltanderson@berkeley.edu>
2017-06-25 13:12:00 -0700

    server building correctly locally, hopefully it will build in the heroku

--- server/index.js
+++ server/index.js
@@ -2,14 +2,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
-// process.env.PWD = process.cwd() || __dirname;
+// process.env.PWD = process.cwd();
+// console.log("The process variable is", process.cwd());
+// console.log("Directory name is: ", __dirname);
 var app = express();
 
 // app.use(express.static(__dirname + '/../react-client/dist'));
 //app.use('/', express.static(__dirname)); 
 process.env.PWD = process.cwd();
 //app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(process.env.PWD, '/../react-client/dist')));
+app.use(express.static(process.env.PWD + '/react-client/dist'));
 
 
 // app.get('/', function(req,res) {
