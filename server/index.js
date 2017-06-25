a0356843ee00e9366e1688536b687c16f81a9955
dandersonstack <danieltanderson@berkeley.edu>
2017-06-25 12:54:52 -0700

    took out default route for index.js

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ var app = express();
 
 app.use(express.static(process.env.PWD + '/../react-client/dist'));
 
-app.get('/', function(req, res) {
-  res.send('hello world!');
-});
+// app.get('/', function(req, res) {
+//   res.send('hello world!');
+// });
 
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
