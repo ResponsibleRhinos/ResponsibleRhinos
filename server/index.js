4d3f201c6db79b2765eed67e4c8f28d36d302c2b
dandersonstack <danieltanderson@berkeley.edu>
2017-06-25 15:01:11 -0700

    fixing index.js to fix merge issues

--- server/index.js
+++ server/index.js
@@ -2,18 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
+process.env.PWD = process.cwd() || __dirname;
 var app = express();
 
-process.env.PWD = process.cwd();
-app.use(express.static(process.env.PWD + '/react-client/dist'));
-
-// app.get('/', function(req, res) {
-//   res.send('hello world!');
-// });
+app.use(express.static(process.env.PWD + '/../react-client/dist'));
 
 app.get('/', function(req, res) {
   res.send('hello world!');
-})
+});
 
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
