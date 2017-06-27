a98f8dba4a0120fc6160b9272e368870bf1f5a29
dandersonstack <danieltanderson@berkeley.edu>
2017-06-26 18:35:16 -0700

    cleaned up models file

--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,6 @@
 var db = require('../db');
 
-// db.query('SELECT ${columns^} FROM ${table~}', {
-//     columns: 'user_name',
-//     table: 'mad_map_users'
-// });
-
 module.exports.users = () => {
   console.log("running query method");
   return db.query('select * from mad_map_users');
 };
-
-// module.exports =
