5fa252a37b26baec75c90e7eaa32a02803c7b608
dandersonstack <danieltanderson@berkeley.edu>
2017-06-26 18:36:21 -0700

    cleaned server index file, ie took out extra comments

--- server/index.js
+++ server/index.js
@@ -15,38 +15,7 @@ app.get('/users', function(req, res) {
   });
 });
 
-// app.get('/', function(req,res) {
-//   res.sendFile('/../react-client/dist');
-// }) 
-// app.get('/', function(req, res) {
-//   res.send('hello world!');
-// });
 
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
 });
-
-
-/**
-var express = require('express');
-var bodyParser = require('body-parser');
-//var models = require('./models');
-
-var port = process.env.PORT || 3000;
-var app = express();
-
-process.env.PWD = process.cwd();
-app.use(express.static(process.env.PWD + '/react-client/dist'));
-
-app.get()
-
-// app.get('/users', function(req,res) {
-//   console.log(models.users);
-//   res.end();
-// }) 
-
-app.listen(port, function() {
-  console.log(`listening on port ${port}!`);
-});
-
-*/
