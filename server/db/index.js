d1118c0e76fdeee29a73f873145b672227247a4c
Hack Reactor Student <student@hackreactor.com>
2017-06-28 12:09:21 -0700

    took out user and database, hopefully heroku will configure for me

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,6 @@ const pgp = require('pg-promise')();
 
 const connection = {
   host: process.env.DATABASE_URL,
-  database: 'mad_map_db',
-  user: 'mad_map_db',
 };
 
 const db = pgp(connection);
--- server/models/index.js
+++ server/models/index.js
@@ -24,30 +24,6 @@ module.exports = {
     
   }
 };
-// var githubId = "dandersonstack";
-// db.query(
-//   )
-// .then((result)=>{
-//   console.log("This was successful: ", result);
-// })
-// .catch((err)=>{
-//   console.log("There was an error", err);
-// });
-
-// INSERT mad_map_users (user_name)
-// SELECT user_name
-// FROM mad_map_users
-// WHERE
-//    NOT EXISTS (SELECT * FROM mad_map_users
-//               WHERE mad_map_users.user_name = c.cName)
-
-// `insert into mad_map_users (user_name)
-//   select ${githubId}
-//   where not exists (
-//       select 1 
-//       from mad_map_users
-//       where mad_map_users.user_name='${githubId}'
-//   );`;
 
 
 
