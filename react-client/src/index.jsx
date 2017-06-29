9cef1b21371724c43e8500e2bd4e539a743d09f4
dandersonstack <danieltanderson@berkeley.edu>
2017-06-29 12:12:59 -0700

    refractoring unecessary components

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,12 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MainMap from './components/map.jsx';
-import PinCreator from './components/pincreator.jsx';
-import PinInfo from './components/pininfo.jsx';
 import Header from './components/header.jsx'; 
 import MapContainer from './components/mapContainer.jsx';
-import Paper from 'material-ui/Paper';
 import UserPage from './components/userpage.jsx';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
